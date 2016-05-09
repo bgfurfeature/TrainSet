1cb2f2f42694f40ea2b31b501fe8bcc88f1284f8
shanghai-Jerry <cjyou20110920sspu@163.com>
2016-05-09 21:04:23 +0800

    + add SVM train model

---
+++ src/main/scala/MainSVM.scala
@@ -0,0 +1,64 @@
+import org.apache.spark.mllib.evaluation.BinaryClassificationMetrics
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.mllib.optimization.SimpleUpdater
+import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.mllib.tree.impurity.Entropy
+import org.apache.spark.{SparkConf, SparkContext}
+import train.DigitRecognizer
+
+/**
+  * Created by C.J.YOU on 2016/5/9.
+  */
+object MainSVM {
+  def main(args: Array[String]) {
+    val sc = new SparkContext(new SparkConf()
+      .setAppName("DigitRe")
+      .setMaster("local")
+    )
+    // checek data
+    val allData = sc.textFile(args(0)).map(_.split(","))
+    val data = allData.map{ lines =>
+      val label = lines(0).toDouble
+      val features = lines.slice(1,lines.length).map(_.toDouble)
+      LabeledPoint(label,Vectors.dense(features))
+    }
+
+    val splitData = data.randomSplit(Array(0.8,0.2))
+    // 使用交叉验证法训练数据和检验数据
+    val trainData = splitData(0).cache()
+    val testData = splitData(1).cache()
+
+
+    // 迭代次数的调优
+    val svmResult  = Seq(1,5,10,50).map { param =>
+      val svmModel = DigitRecognizer.trainSVMWithParams(trainData,0.0,param,new SimpleUpdater,1.0)
+      val predictResult =  testData.map { labeledPoint =>
+        val predicted = svmModel.predict(labeledPoint.features)
+        (predicted,labeledPoint.label)
+      }
+
+      val metrics = new BinaryClassificationMetrics(predictResult)
+      (param,metrics.areaUnderROC())
+    }
+
+    /**
+      *
+      */
+    svmResult.foreach { case (param,acc) =>
+      println(f"param:$param,auc:${acc * 100}")
+    }
+
+    /*val nbBestModel = DigitRecognizer.trainDTWithParams(data,0.001,"multinomial")
+    // predict test.csv filedata
+    val test = sc.textFile(args(1)).map(_.split(","))
+    val testLabels = test.map { lines =>
+      val features = lines.map(_.toDouble)
+      val label = nbBestModel.predict(Vectors.dense(features))
+      label.toInt.toString
+    }
+    val arr = testLabels.zipWithIndex().map{x => (x._2 + 1 +",\""+x._1+"\"")}.collect()
+    FileUtil.writeToFile(args(2),arr)*/
+  }
+
+
+}
